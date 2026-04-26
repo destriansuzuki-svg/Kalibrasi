4a409fe06177c18ba871b6d6a97744d72c78a83c
destriansuzuki-svg <destriansuzuki@gmail.com>
2026-04-26 23:18:06 +0700

    Add calibration data for tools and status

---
+++ data.js
@@ -0,0 +1,24 @@
+
+// Simulasi Data Alat dan Status
+const dataKalibrasi = [
+    {
+        id: "PNT-01",
+        nama: "Air Micrometer",
+        lokasi: "Painting 2W TB1",
+        tglJatuhTempo: "2026-05-10", // 14 hari lagi (akan trigger notif)
+        status: "Proses Kalibrasi",
+        tglDiserahkan: "2026-04-20",
+        tglKembali: "-",
+        pic: "Pak Hudan"
+    },
+    {
+        id: "PNT-02",
+        nama: "Thickness Gauge",
+        lokasi: "Painting 2W TB1",
+        tglJatuhTempo: "2026-06-15",
+        status: "OK",
+        tglDiserahkan: "-",
+        tglKembali: "2025-06-15",
+        pic: "Ibu Tesa"
+    }
+];
